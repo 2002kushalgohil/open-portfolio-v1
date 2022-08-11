88b57025bf8fef0242a688b52ea0704f8665c1b0
Kushal Gohil <2002kushalgohil@gmail.com>
2022-08-11 16:04:39 +0530

    added projects

--- Data/projects.js
+++ Data/projects.js
@@ -2,11 +2,7 @@ export const fullStack = "Full Stack";
 export const uiDevelopment = "UI Development";
 export const webDesign = "Web Design";
 
-export const category = [
-  fullStack,
-  uiDevelopment,
-  // , webDesign
-];
+export const category = [fullStack, uiDevelopment, , webDesign];
 
 export default [
   // ------------------- Full Stack -------------------
@@ -17,15 +13,30 @@ export default [
     stack: ["ReactJS", "NodeJS", "ExpressJS", "MongoDB"],
     liveLink: "https://google-keep--clone.herokuapp.com",
   },
+
+  // ------------------- UI Development -------------------
   {
     name: "Headphone Website",
     imgSrc: "/Projects/Website UI Development/Headphone Website.webp",
     category: uiDevelopment,
     stack: ["NextJS", "TailwindCSS"],
-    liveLink: "https://beats.kushalgohil.com/",
+    liveLink: "https://beats.kushalgohil.com",
+  },
+  {
+    name: "Bridged Media Landing Page",
+    imgSrc: "/Projects/Website UI Development/Bridged Media.webp",
+    category: uiDevelopment,
+    stack: ["NextJS", "TailwindCSS"],
+    liveLink: "https://bridged.media",
   },
 
-  // ------------------- UI Development -------------------
-
   // ------------------- Web Design -------------------
+  {
+    name: "Ghughrawala's Landing Page",
+    imgSrc: "/Projects/Website Design/Ghughrawa wala.webp",
+    category: webDesign,
+    stack: ["Figma"],
+    liveLink:
+      "https://www.figma.com/file/TdeoGExFWhxg5fTmEFmuR9/Untitled?node-id=0%3A1",
+  },
 ];
