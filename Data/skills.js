4555c0c96402ea3d5c6f79b3697760bb65818295
Kushal Gohil <2002kushalgohil@gmail.com>
2024-03-30 09:51:44 +0530

    Fixed Image Issue

--- Data/skills.js
+++ Data/skills.js
@@ -1,66 +1,66 @@
 export default [
   {
-    imgSrc: "/skills/1 Html.svg",
+    imgSrc: "/Skills/1 Html.svg",
     name: "HTML5",
   },
   {
-    imgSrc: "/skills/2 Css.svg",
+    imgSrc: "/Skills/2 Css.svg",
     name: "CSS3",
   },
   {
-    imgSrc: "/skills/3 Tailwind Css.svg",
+    imgSrc: "/Skills/3 Tailwind Css.svg",
     name: "Tailwind-CSS",
   },
   {
-    imgSrc: "/skills/4 Ant Design.svg",
+    imgSrc: "/Skills/4 Ant Design.svg",
     name: "Ant Design",
   },
   {
-    imgSrc: "/skills/5 Figma.svg",
+    imgSrc: "/Skills/5 Figma.svg",
     name: "Figma",
   },
   {
-    imgSrc: "/skills/6 JavaScript.svg",
+    imgSrc: "/Skills/6 JavaScript.svg",
     name: "JavaScript",
   },
   {
-    imgSrc: "/skills/7 TypeScript.svg",
+    imgSrc: "/Skills/7 TypeScript.svg",
     name: "TypeScript",
   },
   {
-    imgSrc: "/skills/8 React JS.svg",
+    imgSrc: "/Skills/8 React JS.svg",
     name: "React JS",
   },
   {
-    imgSrc: "/skills/9 Next JS.svg",
+    imgSrc: "/Skills/9 Next JS.svg",
     name: "Next JS",
   },
   {
-    imgSrc: "/skills/10 Redux.svg",
+    imgSrc: "/Skills/10 Redux.svg",
     name: "Redux",
   },
   {
-    imgSrc: "/skills/Wordpress.svg",
+    imgSrc: "/Skills/Wordpress.svg",
     name: "WordPress",
   },
   {
-    imgSrc: "/skills/11 Node JS.svg",
+    imgSrc: "/Skills/11 Node JS.svg",
     name: "Node JS",
   },
   {
-    imgSrc: "/skills/12 Express JS.svg",
+    imgSrc: "/Skills/12 Express JS.svg",
     name: "Express JS",
   },
   {
-    imgSrc: "/skills/13 Nest JS.svg",
+    imgSrc: "/Skills/13 Nest JS.svg",
     name: "Nest JS",
   },
   {
-    imgSrc: "/skills/14 MongoDB.svg",
+    imgSrc: "/Skills/14 MongoDB.svg",
     name: "MongoDB",
   },
   {
-    imgSrc: "/skills/15 MySQL.svg",
+    imgSrc: "/Skills/15 MySQL.svg",
     name: "MySQL",
   },
 ];
