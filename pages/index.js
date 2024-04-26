38f3ddc48307d949288119cbbdcf870bcffe7fa6
Kushal Gohil <2002kushalgohil@gmail.com>
2024-04-26 15:58:53 +0530

    Added New Font

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
         <title>Kushal Gohil - Full Stack Developer</title>
         <meta name="title" content="Kushal Gohil - Full Stack Developer" />
         <meta name="theme-color" content="#fcda69" />
